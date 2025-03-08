6c46db50b40b106cb49ea3c31d4a68ebdef5bf9a
Chhaya <chayakudas.das56@gmail.com>
2025-03-08 17:25:37 +0530

    Adding exit command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+
+std::vector<std::string> stringSplit(const std::string &input, char delimiter) {
+    std::vector<std::string> tokens;
+    std::stringstream ss(input);
+    std::string token;
+
+    while(std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+bool checkExitCommand(const std::string &command) {
+    std::vector<std::string> tokens = stringSplit(command, ' ');
+    if (tokens.size() != 2) {
+        return false;
+    }
+
+    if (tokens.at(0) != "exit") {
+        return false;
+    }
+
+    if (tokens.at(1) != "0"){
+//        std::cerr <<"Error: invalid exit code: " << tokens.at(1) << std::endl;
+        return false;
+    }
+    return true;
+
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,6 +42,11 @@ int main() {
       std::cout << "$ ";
       std::string input;
       std::getline(std::cin, input);
+
+      if (checkExitCommand(input)) {
+          exit(0);
+      }
+
       std::cout << input <<": command not found "<< std::endl;
   }
 
