58c2f14c7cfa47a366fe1c338aafeb55aa7072a6
Chhaya <chayakudas.das56@gmail.com>
2025-03-08 16:34:10 +0530

    Handling invlaid commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+   std::cout << std::unitbuf;
+   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
    std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
+  std::cout << input <<": command not found "<< std::endl;
+
 }
